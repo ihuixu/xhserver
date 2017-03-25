43769714ac394f0ef34629a40373f85f6fb839d3
ihuixu <xh1021@vip.qq.com>
2017-03-26 03:26:33 +0800

    change server

--- paeonia/middleware/loader.js
+++ paeonia/middleware/loader.js
@@ -1,6 +1,16 @@
+var path = require('path')
 var loader = require('cello-loader')
 
 module.exports = function *(next){
-  this.loader = loader(this.config) 
+  var staticPath = path.join(this.config.hostPath, 'static/config.json')
+
+  try{
+    this.config.static = require(staticPath)
+  
+  }catch(e){
+    this.config.static = {}
+  }
+
+  this.loader = loader(this.config.static) 
   yield next
 }
